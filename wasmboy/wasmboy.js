c18018340fda18d67e29692d9f825d170f900c9e
Aaron Turner <aaron@aaronthedev.com>
2018-02-07 11:12:34 -0800

    Fixed performance, and cleaned up canvas scaling

--- wasmboy/wasmboy.js
+++ wasmboy/wasmboy.js
@@ -1,6 +1,9 @@
 import fetch from 'unfetch'
 import Promise from 'promise-polyfill';
 
+const GAMEBOY_CAMERA_WIDTH = 160;
+const GAMEBOY_CAMERA_HEIGHT = 144;
+
 class WasmBoyLib {
 
   // Start the request to our wasm module
@@ -27,7 +30,12 @@ class WasmBoyLib {
         this.canvasElement = canvasElement;
         this.canvasContext = this.canvasElement.getContext('2d');
         this.canvasContext.clearRect(0, 0, this.canvasElement.width, this.canvasElement.height);
-        this.canvasContext.scale(this.canvasElement.width / 160, this.canvasElement.height / 144);
+        this.canvasImageData = this.canvasContext.createImageData(GAMEBOY_CAMERA_WIDTH, GAMEBOY_CAMERA_HEIGHT);
+
+        // Scale the canvas
+        // https://stackoverflow.com/questions/18547042/resizing-a-canvas-image-without-blurring-it
+        this.canvasContext.imageSmoothingEnabled = false;
+        this.canvasContext.scale(this.canvasElement.width / GAMEBOY_CAMERA_WIDTH, this.canvasElement.height / GAMEBOY_CAMERA_HEIGHT);
       } catch(error) {
         reject(error);
       }
@@ -84,31 +92,52 @@ class WasmBoyLib {
     // Draw the pixels
     // 160x144
     // TODO: Maybe set y back to 144?, works with 143
-    for(let y = 0; y < 143; y++) {
-      for (let x = 0; x < 160; x++) {
 
+    // Split off our image Data
+    const imageDataArray = [];
+
+    for(let y = 0; y < GAMEBOY_CAMERA_HEIGHT - 1; y++) {
+      for (let x = 0; x < GAMEBOY_CAMERA_WIDTH; x++) {
+
+        // Wasm Memory Mapping
         const pixelIndex = 0x10000 + (y * 160) + x;
         const color = this.wasmByteMemory[pixelIndex];
+
+        // Doing graphics using second answer on:
+        // https://stackoverflow.com/questions/4899799/whats-the-best-way-to-set-a-single-pixel-in-an-html5-canvas
+        // Image Data mapping
+        const imageDataIndex = (x + (y * GAMEBOY_CAMERA_WIDTH)) * 4;
+        let rgba = [];
+        const alpha = 255;
+
         if (color) {
-          let fillStyle = false;
           if(color === 1) {
-            fillStyle = "#FFFFFF";
+            rgba = [255, 255, 255, alpha];
           } else if (color === 2) {
-            fillStyle = "#D3D3D3";
+            rgba = [211, 211, 211, alpha];
           } else if (color === 3) {
-            fillStyle = "#A9A9A9";
+            rgba = [169, 169, 169, alpha];
           } else {
-            fillStyle = "#000000";
+            rgba = [0, 0, 0, alpha];
           }
-          this.canvasContext.fillStyle = fillStyle;
-          this.canvasContext.fillRect(x, y, 1, 1);
         } else {
           // TODO: Remove this testing code:
-          this.canvasContext.fillStyle = "#f30000";
-          this.canvasContext.fillRect(x, y, 1, 1);
+          rgba = [255, 0, 0, 1];
+        }
+
+        for(let i = 0; i < rgba.length; i++) {
+          imageDataArray[imageDataIndex + i] = rgba[i];
         }
       }
     }
+
+    // Add our new imageData
+    for(let i = 0; i < imageDataArray.length; i++) {
+      this.canvasImageData.data[i] = imageDataArray[i];
+    }
+    this.canvasContext.putImageData(this.canvasImageData, 0, 0);
+    // drawImage to apply our canvas scale
+    this.canvasContext.drawImage(this.canvasElement, 0, 0);
   }
 
   // Private funciton to returna promise to our wasmModule
