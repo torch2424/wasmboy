e20facaa51d06c3896ccdf29ba03c1b410fda66f
Aaron Turner <aaron@aaronthedev.com>
2018-03-10 20:49:50 -0800

    Did some scaling for the canvas

--- index.js
+++ index.js
@@ -40,48 +40,46 @@ export default class App extends Component {
 		return (
 			<div>
 				<h1>WasmBoy</h1>
-				<div class="systemControls">
+				<div class="wasmboy__systemControls">
 					<WasmBoySystemControls wasmboy={WasmBoy}></WasmBoySystemControls>
 				</div>
 				<div className="wasmboy__canvas-container">
     			<canvas className="wasmboy__canvas-container__canvas"
-            style="border: 1px solid black;"
-            width="640"
-            height="480">
+            style="border: 1px solid black;">
           </canvas>
         </div>
-				<div class="debugger">
+				<div class="wasmboy__debugger">
 					<WasmBoyDebugger wasmboy={WasmBoy} wasmboyGraphics={WasmBoyGraphics} wasmboyAudio={WasmBoyAudio}></WasmBoyDebugger>
 				</div>
 
 				<div class="gamepad">
 
 					{/* DPAD */}
-					<svg id="gamepadDpad" fill="#b967ff" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+					<svg id="gamepadDpad" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
 					    <path d="M0 0h24v24H0z" fill="none"/>
 					    <path d="M15 7.5V2H9v5.5l3 3 3-3zM7.5 9H2v6h5.5l3-3-3-3zM9 16.5V22h6v-5.5l-3-3-3 3zM16.5 9l-3 3 3 3H22V9h-5.5z"/>
 					</svg>
 
 					{/* Start */}
-					<svg id="gamepadStart" fill="#b967ff" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+					<svg id="gamepadStart" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
 					    <path d="M19 13H5v-2h14v2z"/>
 					    <path d="M0 0h24v24H0z" fill="none"/>
 					</svg>
 
 					{/* Select */}
-					<svg id="gamepadSelect" fill="#b967ff" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+					<svg id="gamepadSelect" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
 					    <path d="M19 13H5v-2h14v2z"/>
 					    <path d="M0 0h24v24H0z" fill="none"/>
 					</svg>
 
 					{/* A */}
-					<svg id="gamepadA" fill="#b967ff" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+					<svg id="gamepadA" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
 					    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/>
 					    <path d="M0 0h24v24H0z" fill="none"/>
 					</svg>
 
 					{/* B */}
-					<svg id="gamepadB" fill="#b967ff" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+					<svg id="gamepadB" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
 					    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/>
 					    <path d="M0 0h24v24H0z" fill="none"/>
 					</svg>
