038c8c906d8e6ab1b901fd8a3305d8237d4bb4c4
Aaron Turner <aaron@aaronthedev.com>
2018-03-12 12:14:18 -0700

    Sped up cb opcodes as well

--- wasm/cpu/cbOpcodes.ts
+++ wasm/cpu/cbOpcodes.ts
@@ -63,168 +63,234 @@ export function handleCbOpcode(cbOpcode: u8): i8 {
     instructionRegisterValue = Cpu.registerA;
   }
 
+  // Grab the high nibble to perform skips to speed up performance
+  let opcodeHighNibble = (cbOpcode & 0xF0);
+  opcodeHighNibble = opcodeHighNibble >> 4;
+
   // Send to the correct function
-  if (cbOpcode <= 0x07) {
-    // RLC register 8-bit
-    // Z 0 0 C
-    instructionRegisterResult = rotateRegisterLeft(instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0x0F) {
-    // RRC register 8-bit
-    // Z 0 0 C
-    instructionRegisterResult = rotateRegisterRight(instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0x17) {
-    // RL register 8-bit
-    // Z 0 0 C
-    instructionRegisterResult = rotateRegisterLeftThroughCarry(instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0x1F) {
-    // RR register 8-bit
-    // Z 0 0 C
-    instructionRegisterResult = rotateRegisterRightThroughCarry(instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0x27) {
-    // SLA register 8-bit
-    // Z 0 0 C
-    instructionRegisterResult = shiftLeftRegister(instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0x2F) {
-    // SRA register 8-bit
-    // Z 0 0 0
-    instructionRegisterResult = shiftRightArithmeticRegister(instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0x37) {
-    // SWAP register 8-bit
-    // Z 0 0 0
-    instructionRegisterResult = swapNibblesOnRegister(instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0x3F) {
-    // SRL B
-    // Z 0 0 C
-    instructionRegisterResult = shiftRightLogicalRegister(instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0x47) {
-    // BIT 0,register 8-bit
-    // Z 0 1 -
-    //TODO: Optimize this not to do logic of setting register back
-    instructionRegisterResult = testBitOnRegister(0, instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0x4F) {
-    // BIT 1,register 8-bit
-    // Z 0 1 -
-    instructionRegisterResult = testBitOnRegister(1, instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0x57) {
-    // BIT 2,register 8-bit
-    // Z 0 1 -
-    instructionRegisterResult = testBitOnRegister(2, instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0x5F) {
-    // BIT 3,register 8-bit
-    // Z 0 1 -
-    instructionRegisterResult = testBitOnRegister(3, instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0x67) {
-    // BIT 4,register 8-bit
-    // Z 0 1 -
-    instructionRegisterResult = testBitOnRegister(4, instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0x6F) {
-    // BIT 5,register 8-bit
-    // Z 0 1 -
-    instructionRegisterResult = testBitOnRegister(5, instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0x77) {
-    // BIT 6,register 8-bit
-    // Z 0 1 -
-    instructionRegisterResult = testBitOnRegister(6, instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0x7F) {
-    // BIT 7,register 8-bit
-    // Z 0 1 -
-    instructionRegisterResult = testBitOnRegister(7, instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0x87) {
-    // Res 0,register 8-bit
-    // - - - -
-    instructionRegisterResult = setBitOnRegister(0, 0, instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0x8F) {
-    // Res 1,register 8-bit
-    // - - - -
-    instructionRegisterResult = setBitOnRegister(1, 0, instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0x97) {
-    // Res 2,register 8-bit
-    // - - - -
-    instructionRegisterResult = setBitOnRegister(2, 0, instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0x9F) {
-    // Res 3,register 8-bit
-    // - - - -
-    instructionRegisterResult = setBitOnRegister(3, 0, instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0xA7) {
-    // Res 4,register 8-bit
-    // - - - -
-    instructionRegisterResult = setBitOnRegister(4, 0, instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0xAF) {
-    // Res 5,register 8-bit
-    // - - - -
-    instructionRegisterResult = setBitOnRegister(5, 0, instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0xB7) {
-    // Res 6,register 8-bit
-    // - - - -
-    instructionRegisterResult = setBitOnRegister(6, 0, instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0xBF) {
-    // Res 7,register 8-bit
-    // - - - -
-    instructionRegisterResult = setBitOnRegister(7, 0, instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0xC7) {
-    // SET 0,register 8-bit
-    // - - - -
-    instructionRegisterResult = setBitOnRegister(0, 1, instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0xCF) {
-    // SET 1,register 8-bit
-    // - - - -
-    instructionRegisterResult = setBitOnRegister(1, 1, instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0xD7) {
-    // SET 2,register 8-bit
-    // - - - -
-    instructionRegisterResult = setBitOnRegister(2, 1, instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0xDF) {
-    // SET 3,register 8-bit
-    // - - - -
-    instructionRegisterResult = setBitOnRegister(3, 1, instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0xE7) {
-    // SET 4,register 8-bit
-    // - - - -
-    instructionRegisterResult = setBitOnRegister(4, 1, instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0xEF) {
-    // SET 5,register 8-bit
-    // - - - -
-    instructionRegisterResult = setBitOnRegister(5, 1, instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0xF7) {
-    // SET 6,register 8-bit
-    // - - - -
-    instructionRegisterResult = setBitOnRegister(6, 1, instructionRegisterValue);
-    handledOpcode = true;
-  } else if (cbOpcode <= 0xFF) {
-    // SET 7,register 8-bit
-    // - - - -
-    instructionRegisterResult = setBitOnRegister(7, 1, instructionRegisterValue);
-    handledOpcode = true;
+  if (opcodeHighNibble === 0x00) {
+    if (cbOpcode <= 0x07) {
+      // RLC register 8-bit
+      // Z 0 0 C
+      instructionRegisterResult = rotateRegisterLeft(instructionRegisterValue);
+      handledOpcode = true;
+    } else if (cbOpcode <= 0x0F) {
+      // RRC register 8-bit
+      // Z 0 0 C
+      instructionRegisterResult = rotateRegisterRight(instructionRegisterValue);
+      handledOpcode = true;
+    }
+  }
+
+  if (opcodeHighNibble === 0x01) {
+    if (cbOpcode <= 0x17) {
+      // RL register 8-bit
+      // Z 0 0 C
+      instructionRegisterResult = rotateRegisterLeftThroughCarry(instructionRegisterValue);
+      handledOpcode = true;
+    } else if (cbOpcode <= 0x1F) {
+      // RR register 8-bit
+      // Z 0 0 C
+      instructionRegisterResult = rotateRegisterRightThroughCarry(instructionRegisterValue);
+      handledOpcode = true;
+    }
+  }
+
+  if (opcodeHighNibble === 0x02) {
+    if (cbOpcode <= 0x27) {
+      // SLA register 8-bit
+      // Z 0 0 C
+      instructionRegisterResult = shiftLeftRegister(instructionRegisterValue);
+      handledOpcode = true;
+    } else if (cbOpcode <= 0x2F) {
+      // SRA register 8-bit
+      // Z 0 0 0
+      instructionRegisterResult = shiftRightArithmeticRegister(instructionRegisterValue);
+      handledOpcode = true;
+    }
+  }
+
+  if (opcodeHighNibble === 0x03) {
+    if (cbOpcode <= 0x37) {
+      // SWAP register 8-bit
+      // Z 0 0 0
+      instructionRegisterResult = swapNibblesOnRegister(instructionRegisterValue);
+      handledOpcode = true;
+    } else if (cbOpcode <= 0x3F) {
+      // SRL B
+      // Z 0 0 C
+      instructionRegisterResult = shiftRightLogicalRegister(instructionRegisterValue);
+      handledOpcode = true;
+    }
+  }
+
+  if (opcodeHighNibble === 0x04) {
+    if (cbOpcode <= 0x47) {
+      // BIT 0,register 8-bit
+      // Z 0 1 -
+      //TODO: Optimize this not to do logic of setting register back
+      instructionRegisterResult = testBitOnRegister(0, instructionRegisterValue);
+      handledOpcode = true;
+    } else if (cbOpcode <= 0x4F) {
+      // BIT 1,register 8-bit
+      // Z 0 1 -
+      instructionRegisterResult = testBitOnRegister(1, instructionRegisterValue);
+      handledOpcode = true;
+    }
+  }
+
+  if (opcodeHighNibble === 0x05) {
+    if (cbOpcode <= 0x57) {
+      // BIT 2,register 8-bit
+      // Z 0 1 -
+      instructionRegisterResult = testBitOnRegister(2, instructionRegisterValue);
+      handledOpcode = true;
+    } else if (cbOpcode <= 0x5F) {
+      // BIT 3,register 8-bit
+      // Z 0 1 -
+      instructionRegisterResult = testBitOnRegister(3, instructionRegisterValue);
+      handledOpcode = true;
+    }
+  }
+
+  if (opcodeHighNibble === 0x06) {
+    if (cbOpcode <= 0x67) {
+      // BIT 4,register 8-bit
+      // Z 0 1 -
+      instructionRegisterResult = testBitOnRegister(4, instructionRegisterValue);
+      handledOpcode = true;
+    } else if (cbOpcode <= 0x6F) {
+      // BIT 5,register 8-bit
+      // Z 0 1 -
+      instructionRegisterResult = testBitOnRegister(5, instructionRegisterValue);
+      handledOpcode = true;
+    }
+  }
+
+  if (opcodeHighNibble === 0x07) {
+    if (cbOpcode <= 0x77) {
+      // BIT 6,register 8-bit
+      // Z 0 1 -
+      instructionRegisterResult = testBitOnRegister(6, instructionRegisterValue);
+      handledOpcode = true;
+    } else if (cbOpcode <= 0x7F) {
+      // BIT 7,register 8-bit
+      // Z 0 1 -
+      instructionRegisterResult = testBitOnRegister(7, instructionRegisterValue);
+      handledOpcode = true;
+    }
+  }
+
+  if (opcodeHighNibble === 0x08) {
+    if (cbOpcode <= 0x87) {
+      // Res 0,register 8-bit
+      // - - - -
+      instructionRegisterResult = setBitOnRegister(0, 0, instructionRegisterValue);
+      handledOpcode = true;
+    } else if (cbOpcode <= 0x8F) {
+      // Res 1,register 8-bit
+      // - - - -
+      instructionRegisterResult = setBitOnRegister(1, 0, instructionRegisterValue);
+      handledOpcode = true;
+    }
+  }
+
+  if (opcodeHighNibble === 0x09) {
+    if (cbOpcode <= 0x97) {
+      // Res 2,register 8-bit
+      // - - - -
+      instructionRegisterResult = setBitOnRegister(2, 0, instructionRegisterValue);
+      handledOpcode = true;
+    } else if (cbOpcode <= 0x9F) {
+      // Res 3,register 8-bit
+      // - - - -
+      instructionRegisterResult = setBitOnRegister(3, 0, instructionRegisterValue);
+      handledOpcode = true;
+    }
+  }
+
+  if (opcodeHighNibble === 0x0A) {
+    if (cbOpcode <= 0xA7) {
+      // Res 4,register 8-bit
+      // - - - -
+      instructionRegisterResult = setBitOnRegister(4, 0, instructionRegisterValue);
+      handledOpcode = true;
+    } else if (cbOpcode <= 0xAF) {
+      // Res 5,register 8-bit
+      // - - - -
+      instructionRegisterResult = setBitOnRegister(5, 0, instructionRegisterValue);
+      handledOpcode = true;
+    }
+  }
+
+  if (opcodeHighNibble === 0x0B) {
+    if (cbOpcode <= 0xB7) {
+      // Res 6,register 8-bit
+      // - - - -
+      instructionRegisterResult = setBitOnRegister(6, 0, instructionRegisterValue);
+      handledOpcode = true;
+    } else if (cbOpcode <= 0xBF) {
+      // Res 7,register 8-bit
+      // - - - -
+      instructionRegisterResult = setBitOnRegister(7, 0, instructionRegisterValue);
+      handledOpcode = true;
+    }
+  }
+
+  if (opcodeHighNibble === 0x0C) {
+    if (cbOpcode <= 0xC7) {
+      // SET 0,register 8-bit
+      // - - - -
+      instructionRegisterResult = setBitOnRegister(0, 1, instructionRegisterValue);
+      handledOpcode = true;
+    } else if (cbOpcode <= 0xCF) {
+      // SET 1,register 8-bit
+      // - - - -
+      instructionRegisterResult = setBitOnRegister(1, 1, instructionRegisterValue);
+      handledOpcode = true;
+    }
+  }
+
+  if (opcodeHighNibble === 0x0D) {
+    if (cbOpcode <= 0xD7) {
+      // SET 2,register 8-bit
+      // - - - -
+      instructionRegisterResult = setBitOnRegister(2, 1, instructionRegisterValue);
+      handledOpcode = true;
+    } else if (cbOpcode <= 0xDF) {
+      // SET 3,register 8-bit
+      // - - - -
+      instructionRegisterResult = setBitOnRegister(3, 1, instructionRegisterValue);
+      handledOpcode = true;
+    }
+  }
+
+  if (opcodeHighNibble === 0x0E) {
+    if (cbOpcode <= 0xE7) {
+      // SET 4,register 8-bit
+      // - - - -
+      instructionRegisterResult = setBitOnRegister(4, 1, instructionRegisterValue);
+      handledOpcode = true;
+    } else if (cbOpcode <= 0xEF) {
+      // SET 5,register 8-bit
+      // - - - -
+      instructionRegisterResult = setBitOnRegister(5, 1, instructionRegisterValue);
+      handledOpcode = true;
+    }
+  }
+
+  if (opcodeHighNibble === 0x0F) {
+    if (cbOpcode <= 0xF7) {
+      // SET 6,register 8-bit
+      // - - - -
+      instructionRegisterResult = setBitOnRegister(6, 1, instructionRegisterValue);
+      handledOpcode = true;
+    } else if (cbOpcode <= 0xFF) {
+      // SET 7,register 8-bit
+      // - - - -
+      instructionRegisterResult = setBitOnRegister(7, 1, instructionRegisterValue);
+      handledOpcode = true;
+    }
   }
 
   // Finally Pass back into the correct register
