c669dda8061bfc90351476771afabef24e463597
Aaron Turner <aaron@aaronthedev.com>
2018-03-13 02:20:30 -0700

    Did some more random improvements in the JS side of things

--- wasm/memory/memoryMap.ts
+++ wasm/memory/memoryMap.ts
@@ -14,16 +14,17 @@ import {
 // To the wasmboy memory map
 // Following: http://gameboy.mongenel.com/dmg/asmmemmap.html
 // And https://github.com/Dooskington/GameLad/wiki/Part-11---Memory-Bank-Controllers
-export function getWasmBoyOffsetFromGameBoyOffset(gameboyOffset: u32): u32 {
+export function getWasmBoyOffsetFromGameBoyOffset(gameboyOffset: u16): i32 {
 
   // Wasmboy offset
-  let wasmboyOffset: u32 = Memory.gameBoyInternalMemoryLocation;
+  let wasmboyOffset: i32 = Memory.gameBoyInternalMemoryLocation;
+  let largeGameboyOffset: i32 = gameboyOffset;
 
   // Find the wasmboy offser
   if(gameboyOffset < Memory.switchableCartridgeRomLocation) {
     // Cartridge ROM - Bank 0 (fixed)
     // 0x0000 -> 0x073800
-    wasmboyOffset = gameboyOffset + Memory.gameBytesLocation;
+    wasmboyOffset = largeGameboyOffset + Memory.gameBytesLocation;
   } else if(gameboyOffset < Memory.videoRamLocation) {
     // Cartridge ROM - Switchable Banks 1-xx
     // 0x4000 -> (0x073800 + 0x4000)
@@ -31,7 +32,7 @@ export function getWasmBoyOffsetFromGameBoyOffset(gameboyOffset: u32): u32 {
   } else if (gameboyOffset < Memory.cartridgeRamLocation) {
     // Video RAM
     // 0x8000 -> 0x000400
-    wasmboyOffset = (gameboyOffset - Memory.videoRamLocation) + Memory.gameBoyInternalMemoryLocation;
+    wasmboyOffset = (largeGameboyOffset - Memory.videoRamLocation) + Memory.gameBoyInternalMemoryLocation;
   } else if (gameboyOffset < Memory.internalRamBankZeroLocation) {
     // Cartridge RAM - A.K.A External RAM
     // 0xA000 -> 0x008400
@@ -39,7 +40,7 @@ export function getWasmBoyOffsetFromGameBoyOffset(gameboyOffset: u32): u32 {
   } else {
     // NOTE / TODO: Switchable Internal Ram Banks?
     // 0xC000 -> 0x000400
-    wasmboyOffset = (gameboyOffset - Memory.videoRamLocation) + Memory.gameBoyInternalMemoryLocation;
+    wasmboyOffset = (largeGameboyOffset - Memory.videoRamLocation) + Memory.gameBoyInternalMemoryLocation;
   }
 
   return wasmboyOffset;
