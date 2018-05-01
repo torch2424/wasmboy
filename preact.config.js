8a095fa1624ec69dd499c8122f3658c7cbdcd53d
Aaron Turner <aaron@aaronthedev.com>
2018-04-30 20:12:42 -0700

    Added our preact config to prettier

--- preact.config.js
+++ preact.config.js
@@ -6,32 +6,31 @@
  * @param {object} env - options passed to CLI.
  * @param {WebpackConfigHelpers} helpers - object with useful helpers when working with config.
  **/
-export default function (config, env, helpers) {
-
+export default function(config, env, helpers) {
   if (env.production) {
     // https://stackoverflow.com/questions/45870467/error-in-bundle-js-from-uglifyjs-name-expected
-   const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+    const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
-   // Remove the other uglify JS plugin
-   let uglifyIndex = -1;
-   config.plugins.some((plugin, index) => {
-     if (plugin.options && plugin.options.mangle) {
-       uglifyIndex = index;
-       return true;
-     }
-     return false;
-   });
+    // Remove the other uglify JS plugin
+    let uglifyIndex = -1;
+    config.plugins.some((plugin, index) => {
+      if (plugin.options && plugin.options.mangle) {
+        uglifyIndex = index;
+        return true;
+      }
+      return false;
+    });
 
-   if (uglifyIndex >= 0) {
-     config.plugins.splice(uglifyIndex, 1);
-     config.plugins.push(new UglifyJSPlugin());
-   }
+    if (uglifyIndex >= 0) {
+      config.plugins.splice(uglifyIndex, 1);
+      config.plugins.push(new UglifyJSPlugin());
+    }
 
-   // Also, change our outpout directory
-   // serving on github pages, so: wasmBoy/
-   // https://github.com/developit/preact-cli/issues/218
-   // https://github.com/developit/preact-cli/pull/323
-   config.output.publicPath = "/wasmBoy/";
+    // Also, change our outpout directory
+    // serving on github pages, so: wasmBoy/
+    // https://github.com/developit/preact-cli/issues/218
+    // https://github.com/developit/preact-cli/pull/323
+    config.output.publicPath = '/wasmBoy/';
   }
 
   // Add a wasm loader
@@ -45,5 +44,5 @@ export default function (config, env, helpers) {
       mimetype: 'application/wasm'
     }
   });
-  config.resolve.extensions.push(".wasm");
+  config.resolve.extensions.push('.wasm');
 }
