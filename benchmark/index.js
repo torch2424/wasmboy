16a939622bb6109dd46cab70c2617a6f22c3ec8c
Aaron Turner <aaron@aaronthedev.com>
2018-11-02 02:45:51 -0700

    Left notes on what we're gonna do for benchmarking

---
+++ benchmark/index.js
@@ -0,0 +1,19 @@
+/* 
+
+Benchmarking Plans:
+
+ESBench doesn't support my entire iife :(
+
+So we'll have to roll our own.
+
+Use rollup plugin serve, and just make a simple Preact component page.
+
+Similar to Assemblyscript docz, and AMP localhost :)
+
+Definitly use benchmarkJS. And then use the microtime for our performance.
+
+Find some lightweight charting lib.
+
+Deploy all this, as well as alternative demos to wasmboy.app. Under subdirectories.
+
+*/
--- rollup.config.js
+++ rollup.config.js
@@ -3,17 +3,11 @@ import libBundles from './rollup.lib';
 import workerBundles from './rollup.worker';
 import coreTsBundles from './rollup.core';
 import getCoreBundles from './rollup.getcore';
-import benchmarkBundles from './rollup.benchmark';
 
 let exports = [];
-
-if (process.env.BENCHMARK) {
-  exports = [...benchmarkBundles];
-} else {
-  exports = [...getCoreBundles, ...workerBundles, ...libBundles];
-  if (process.env.TS) {
-    exports = [...coreTsBundles, ...exports];
-  }
+exports = [...getCoreBundles, ...workerBundles, ...libBundles];
+if (process.env.TS) {
+  exports = [...coreTsBundles, ...exports];
 }
 
 export default exports;
