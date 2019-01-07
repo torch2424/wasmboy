12587684ffca513805d21d447a93570f9f30764a
Aaron Turner <aaron@aaronthedev.com>
2019-01-06 17:04:32 -0800

    Started the canvas on load overlay

--- demo/debugger/components/mobile/mobile.js
+++ demo/debugger/components/mobile/mobile.js
@@ -1,6 +1,7 @@
 // Base Component to handle showing mobile UIs
 
 import { h, Component } from 'preact';
+import { WasmBoy } from '../../wasmboy';
 import { Pubx } from 'pubx';
 import { PUBX_KEYS } from '../../pubx.config';
 
@@ -12,7 +13,7 @@ export default class Mobile extends Component {
   constructor() {
     super();
 
-    Pubx.subscribe(PUBX_KEYS.MOBILE, newState => this.setState(newState));
+    Pubx.subscribe(PUBX_KEYS.WASMBOY, newState => this.setState(newState));
 
     window.addEventListener('resize', () => {
       Pubx.get(PUBX_KEYS.MOBILE).update();
@@ -28,11 +29,21 @@ export default class Mobile extends Component {
   componentDidMount() {}
 
   render() {
+    let wasmboyInfo = '';
+    if (!WasmBoy.isLoadedAndStarted()) {
+      wasmboyInfo = (
+        <div class="mobile-container__info-container__info">
+          <h1>Yo!</h1>
+        </div>
+      );
+    }
+
     return (
       <div class="mobile-container">
         <div class="mobile-container__canvas-container">
           <canvas id="mobile-container__wasmboy-canvas" class="pixel-canvas" />
         </div>
+        <div class="mobile-container__info-container">{wasmboyInfo}</div>
         <Touchpad />
       </div>
     );
