a0c7f49594d37b4aef5b983929f30810097dc7dd
Aaron Turner <aaron@aaronthedev.com>
2019-02-18 03:16:24 -0800

    Moved the colors into a constants, getting sleepy

---
+++ core/graphics/colors.constants.ts
@@ -0,0 +1,22 @@
+// File for all of the colors for different GB Palletes
+
+// Our default colors
+export class DefaultColor {
+  // Obj 0
+  static readonly obj0White: i32 = 0xf2f2f2;
+  static readonly obj0LightGrey: i32 = 0xa0a0a0;
+  static readonly obj0DarkGrey: i32 = 0x585858;
+  static readonly obj0Black: i32 = 0x080808;
+
+  // Obj1
+  static readonly obj1White: i32 = 0xf2f2f2;
+  static readonly obj1LightGrey: i32 = 0xa0a0a0;
+  static readonly obj1DarkGrey: i32 = 0x585858;
+  static readonly obj1Black: i32 = 0x080808;
+
+  //Bg
+  static readonly bgWhite: i32 = 0xf2f2f2;
+  static readonly bgLightGrey: i32 = 0xa0a0a0;
+  static readonly bgDarkGrey: i32 = 0x585858;
+  static readonly bglack: i32 = 0x080808;
+}
