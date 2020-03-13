abb85b21797528b40eeaeb5519a3003895031fc7
Aaron Turner <aaron@aaronthedev.com>
2020-03-13 02:25:35 -0700

    Added analytics to Iframe Embed

--- demo/iframe/App.svelte
+++ demo/iframe/App.svelte
@@ -1,11 +1,28 @@
 <script>
-
   import PlayPoster from './components/PlayPoster.svelte'; 
   import WasmBoy from './components/WasmBoy.svelte'; 
   import Modal from './components/Modal.svelte';
   import ControlsBar from './components/ControlsBar.svelte'; 
-
   import {isStarted, isLoaded} from './stores.js';
+
+  import loadScript from 'load-script';
+
+  // Load our analytics
+  if (typeof window !== 'undefined') {
+    loadScript('https://www.googletagmanager.com/gtag/js?id=UA-125276735-3', (err, script) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        window.dataLayer.push(arguments);
+      }
+      gtag('js', new Date());
+      gtag('config', 'UA-125276735-3');
+    });
+  }
 </script>
 
 <main class="app">
